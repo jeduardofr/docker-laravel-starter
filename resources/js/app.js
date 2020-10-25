2dc92af96f4e0d0f6c30223a3202bf842f356391
Eduardo Fuentes <jeduardo.fuentes@alumnos.udg.mx>
2020-10-25 14:08:36 -0600

    Added React Support with only-js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-require('./bootstrap');
+import React from "react";
+
+function App() {
+    return (
+        <main>
+            <h1>Hello World</h1>
+        </main>
+    );
+}
+
+export default App;
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +11,13 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.react("resources/js/index.js", "public/js").browserSync({
+    open: "external",
+    proxy: "webserver",
+    files: ["resources/views/**/*.php", "public/js/*.js"],
+    notify: false
+});
+
+if (mix.inProduction()) {
+    mix.version();
+}
